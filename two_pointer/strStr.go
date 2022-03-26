dfd0be557d9993bb53404ba95c05474f0a17a90e
cuong tran <bacuongtr@gmail.com>
2022-03-26 20:00:02 +0700

    strStr

---
+++ two_pointer/strStr.go
@@ -0,0 +1,16 @@
+package twopointer
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
